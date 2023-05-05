a7e56438c5acc0683da8f3345c9a9999039ae661
swatidurgekar <swatidurgekar@gmail.com>
2023-05-05 09:31:01 +0530

    dynamic styling

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import "./Button.css";
+
+const Button = (props) => {
+  return (
+    <button
+      type={props.type}
+      className={!props.isValid ? "button-invalid" : "button"}
+      onClick={props.onClick}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
